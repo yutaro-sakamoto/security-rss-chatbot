cd0429e05e7fadfa5c559642e8ad6dc5d215fbfc
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-21 23:10:06 +0900

    fix: add missing files and enable cdk-nag

--- bin/security-rss-chatbot.ts
+++ bin/security-rss-chatbot.ts
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import { SecurityRssChatbotStack } from "../lib/security-rss-chatbot-stack";
+import { AwsSolutionsChecks, NagSuppressions } from "cdk-nag";
+import { Aspects } from "aws-cdk-lib";
 
 const app = new cdk.App();
-new SecurityRssChatbotStack(app, "SecurityRssChatbotStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+const stack = new SecurityRssChatbotStack(app, "SecurityRssChatbotStack", {});
+
+NagSuppressions.addStackSuppressions(stack, [
+  {
+    id: "AwsSolutions-S1",
+    reason: "Server access logs of S3 buckets are not required",
+  },
+  { id: "AwsSolutions-IAM4", reason: "Ignore warnings of IAM policies" },
+  { id: "AwsSolutions-IAM5", reason: "Ignore warnings of IAM policies" },
+]);
---
+++ lib/RSSCollector/RssCollector.function.ts
@@ -0,0 +1,24 @@
+import { S3 } from "aws-sdk";
+import fetch from "node-fetch";
+
+const s3 = new S3();
+const BUCKET_NAME = process.env.BUCKET_NAME!;
+
+export const handler = async () => {
+  try {
+    const rssUrl = "https://www.jpcert.or.jp/rss/jpcert.rdf";
+    const response = await fetch(rssUrl);
+    const rssText = await response.text();
+    await s3
+      .putObject({
+        Bucket: BUCKET_NAME,
+        Key: `rss-${Date.now()}.xml`,
+        Body: rssText,
+        ContentType: "application/rss+xml",
+      })
+      .promise();
+    return { statusCode: 200, body: "Success" };
+  } catch (_err) {
+    return { statusCode: 500, body: "fail" };
+  }
+};
