4a6cacb82bca8e60c04596441ce6290abd18c709
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-21 21:19:20 +0900

    feat: fetch an rss feed hourly

--- lib/security-rss-chatbot-stack.ts
+++ lib/security-rss-chatbot-stack.ts
@@ -1,16 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { RssCollector } from "./RSSCollector/RssCollector";
 export class SecurityRssChatbotStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SecurityRssChatbotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new RssCollector(this, "RssCollector");
   }
 }
