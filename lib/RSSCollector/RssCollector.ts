4166c15f416df426d3964702a42c67fadad6e5b4
yutaro-sakamoto <80912876+yutaro-sakamoto@users.noreply.github.com>
2025-01-24 21:09:21 +0900

    refactor: introduce props into RssCollector (#4)

--- lib/RSSCollector/RssCollector.ts
+++ lib/RSSCollector/RssCollector.ts
@@ -7,28 +7,33 @@ import * as events from "aws-cdk-lib/aws-events";
 import * as targets from "aws-cdk-lib/aws-events-targets";
 
 /**
+ * Properties for RssCollector
+ */
+export interface RssCollectorProps {
+  /**
+   * The S3 bucket to store RSS data
+   */
+  readonly bucket: s3.Bucket;
+}
+
+/**
  * A stack includes the following resources
  * * Lambda function that collects RSS data
  * * S3 bucket to store RSS data
  * * EventBridge rule to trigger the Lambda function every hour
  */
 export class RssCollector extends Construct {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: RssCollectorProps) {
     super(scope, id);
-
-    const bucket = new s3.Bucket(this, "RssDataBucket", {
-      lifecycleRules: [{ expiration: Duration.days(30) }],
-      enforceSSL: true,
-    });
 
     const rssCollectorFn = new NodejsFunction(this, "function", {
       runtime: lambda.Runtime.NODEJS_22_X,
       environment: {
-        BUCKET_NAME: bucket.bucketName,
+        BUCKET_NAME: props.bucket.bucketName,
       },
     });
 
-    bucket.grantPut(rssCollectorFn);
+    props.bucket.grantPut(rssCollectorFn);
 
     new events.Rule(this, "RssCollectorSchedule", {
       schedule: events.Schedule.rate(Duration.minutes(60)),
