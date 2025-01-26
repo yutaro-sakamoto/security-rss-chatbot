bdd12b592554ce03027ba75a2347e7c84b2a8ee8
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2025-01-26 19:59:19 +0900

    change: add bedrock knowledge base using generative-ai-cdk-constructs

--- lib/Bedrock/Bedrock.ts
+++ lib/Bedrock/Bedrock.ts
@@ -1,8 +1,7 @@
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 //import * as bedrock from "aws-cdk-lib/aws-bedrock";
-import * as aws_iam from "aws-cdk-lib/aws-iam";
-import * as opensearchserverless from "aws-cdk-lib/aws-opensearchserverless";
+import { bedrock } from "@cdklabs/generative-ai-cdk-constructs";
 
 /**
  * Properties for RssCollector
@@ -23,150 +22,21 @@ export interface BedRockProps {
 export class BedRock extends Construct {
   constructor(scope: Construct, id: string, props: BedRockProps) {
     super(scope, id);
-    const modelArn =
-      "arn:aws:bedrock:ap-northeast-1::foundation-model/anthropic.claude-3-5-sonnet-20240620-v1:0";
-    //const role =
-    new aws_iam.Role(this, `KnowledgeBaseRole`, {
-      roleName: `knowledgeBaseRole`,
-      assumedBy: new aws_iam.ServicePrincipal("bedrock.amazonaws.com"),
-      inlinePolicies: {
-        inlinePolicy1: new aws_iam.PolicyDocument({
-          statements: [
-            new aws_iam.PolicyStatement({
-              resources: [modelArn],
-              actions: ["bedrock:InvokeModel"],
-            }),
-            new aws_iam.PolicyStatement({
-              resources: [
-                props.bucket.bucketArn,
-                `${props.bucket.bucketArn}/*`,
-              ],
-              actions: ["s3:ListBucket", "s3:GetObject"],
-            }),
-          ],
-        }),
-      },
+    const kb = new bedrock.KnowledgeBase(this, "KnowledgeBase", {
+      embeddingsModel: bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
+      instruction:
+        "Use this knowledge base to answer questions about books. " +
+        "It contains the full text of novels.",
     });
 
-    const aossFullAccessRole = new aws_iam.Role(this, `aossFullAccessPolicy`, {
-      roleName: `aossFullAccessRole`,
-      assumedBy: new aws_iam.ServicePrincipal("bedrock.amazonaws.com"),
-      inlinePolicies: {
-        inlinePolicy1: new aws_iam.PolicyDocument({
-          statements: [
-            new aws_iam.PolicyStatement({
-              resources: ["*"],
-              actions: ["aoss:*"],
-            }),
-          ],
-        }),
-      },
+    new bedrock.S3DataSource(this, "DataSource", {
+      bucket: props.bucket,
+      knowledgeBase: kb,
+      dataSourceName: "books",
+      chunkingStrategy: bedrock.ChunkingStrategy.fixedSize({
+        maxTokens: 500,
+        overlapPercentage: 20,
+      }),
     });
-
-    // Create a collection for searching
-    const collection = new opensearchserverless.CfnCollection(
-      this,
-      "Collection",
-      {
-        name: "knowledge-base",
-        type: "VECTORSEARCH",
-        standbyReplicas: "DISABLED",
-      },
-    );
-
-    // Encryption security config for collection
-    const collectionSecurityPolicy = new opensearchserverless.CfnSecurityPolicy(
-      this,
-      "AOSSEncryptionPolicy",
-      {
-        name: "knowledge-base-encryption-policy",
-        type: "encryption",
-        policy: JSON.stringify({
-          Rules: [
-            {
-              ResourceType: "collection",
-              Resource: [`collection/${collection.name}`],
-            },
-          ],
-          AWSOwnedKey: true,
-        }),
-      },
-    );
-
-    collection.addDependency(collectionSecurityPolicy);
-
-    // Network security config for collection
-    new opensearchserverless.CfnSecurityPolicy(this, "AOSSNetworkPolicy", {
-      name: "knowledge-base-network-policy",
-      type: "network",
-      policy: JSON.stringify([
-        {
-          Rules: [
-            {
-              ResourceType: "collection",
-              Resource: [`collection/${collection.name}`],
-            },
-            {
-              ResourceType: "dashboard",
-              Resource: [`collection/${collection.name}`],
-            },
-          ],
-          AllowFromPublic: false,
-          SourceServices: ["bedrock.amazonaws.com"],
-        },
-      ]),
-    });
-
-    // Network security config for collection
-    new opensearchserverless.CfnAccessPolicy(this, "AOSSAccessPolicy", {
-      name: "knowledge-base-access-policy",
-      type: "data",
-      policy: JSON.stringify([
-        {
-          Rules: [
-            {
-              ResourceType: "collection",
-              Resource: [`collection/${collection.name}`],
-              Permission: ["aoss:*"],
-            },
-            {
-              ResourceType: "index",
-              Resource: [`index/${collection.name}/*`],
-              Permission: ["aoss:*"],
-            },
-          ],
-          Principal: [aossFullAccessRole.roleArn],
-        },
-      ]),
-    });
-
-    //// Resource policy granting access
-    //new opensearchserverless.CfnAccessPolicy(this, "CollectionPolicy", {
-    //  name: "my-opensearch-policy",
-    //  type: "data",
-    //  policy: JSON.stringify({
-    //    Version: "2012-10-17",
-    //    Statement: [
-    //      {
-    //        Effect: "Allow",
-    //        Principal: { AWS: "*" }, // Adjust to your needs
-    //        Action: ["os:ESHttp*"],
-    //        Resource: `arn:aws:opensearchserverless:${this.region}:${this.account}:collection/${collection.name}/*`
-    //      }
-    //    ]
-    //  })
-    //});
-
-    //new bedrock.CfnKnowledgeBase(this, "KnowledgeBase", {
-    //    name: "KnowledgeBase",
-
-    //    roleArn: role.roleArn,
-    //    knowledgeBaseConfiguration: {
-    //        type: 'VECTOR',
-    //        vectorKnowledgeBaseConfiguration: {
-    //            embeddingModelArn: modelArn,
-    //        }
-    //    },
-    //});
   }
 }
